a781360a6b7e3da6045d403e57f13164512a1e23
postmelee <meleeisdeveloping@gmail.com>
2020-09-19 04:57:02 +0900

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Dare from "./components/dare-component";
+import { StyleSheet, ScrollView, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.header}>
+        <Text>
+          Head
+        </Text>
+      </View>
+      
+      <ScrollView style={{flex: 1, }}>
+      <View style={styles.title}>
+        <Text style={styles.titleText}>
+          Appointment
+        </Text>
+      </View>
+        <View style={styles.list}>
+          <Dare side='left'></Dare>
+          <Dare side='right'></Dare>
+        </View>
+        <View style={styles.list}>
+          <Dare side='left'></Dare>
+          <Dare side='right'></Dare>
+        </View>
+        <View style={styles.list}>
+          <Dare side='left'></Dare>
+          <Dare side='right'></Dare>
+        </View>
+        <View style={styles.list}>
+          <Dare side='left'></Dare>
+          <Dare side='right'></Dare>
+        </View>
+        <View style={styles.list}>
+          <Dare side='left'></Dare>
+          <Dare side='right'></Dare>
+        </View>
+        <View style={styles.list}>
+          <Dare side='left'></Dare>
+          <Dare side='right'></Dare>
+        </View>
+        
+
+      
+      </ScrollView>
+      <View style={styles.navbar}>
+        <Text>
+          Navbar
+        </Text>
+      </View>
     </View>
   );
 }
@@ -15,7 +60,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    flex: 0.1,
+    backgroundColor: '#aaa',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    width: '100%',
+
+    backgroundColor: '#bbb',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 40,
+  },
+  content: {
+    width: '100%',
+    flex: 0.6,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+
+    alignItems: "center",
+  },
+  navbar: {
+    flex: 0.1,
+    backgroundColor: '#ddd',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  list: {
+    marginTop: '4%',
+    flexDirection: 'row',
+    
+  },
+
 });
+
